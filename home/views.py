1b2697a088b817c5e75049cb77903f2b610bc6db
mohsha313 <159111963+mohsha313@users.noreply.github.com>
2024-04-29 17:22:55 -0700

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,12 @@
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def calculator(request):
+	if request.method == 'POST':
+		result = request.POST.get('result', '')
+
+		return render(request, 'index.html', {'result': result})
+
+	return render(request, 'index.html')
